8f8602cc354e13ef4a9b097a4223973c3ae058da
praveenaa47 <pravipraveena722@gmail.com>
2025-12-05 16:43:21 +0530

    latestpull

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://sparego-backend.onrender.com/api";
-export const BASE_URL = "https://sparego.in/api";
+export const BASE_URL = "https://sparego-backend.onrender.com/api";
+// export const BASE_URL = "https://sparego.in/api";
 
 // export const BASE_URL = "http://localhost:9000/api";
 export const IMG_URL = "https://res.cloudinary.com/dgll2kfzv/image/upload/";
